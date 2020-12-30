0b3475a659c16d7b88e5aceb0c4c34ea2f55dfe9
syu-akita <syu.akita.0612@gmail.com>
2020-12-30 14:12:41 +0900

    Userモデル、バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,            presence: true
-  validates :last_name,           presence: true
-  validates :first_name,          presence: true
-  validates :last_name_k,         presence: true
-  validates :first_name_k,        presence: true
   validates :birthday,            presence: true
+  validates :password_confirmation, presence: true
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    :last_name
+    :first_name
+    :last_name_k
+    :first_name_k
+  end
+
+  
 end
