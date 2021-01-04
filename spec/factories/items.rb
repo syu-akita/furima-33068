f5bfcc6a1aea91697a701cb3c250ff0bd86b43e5
syu-akita <syu.akita.0612@gmail.com>
2021-01-04 19:24:56 +0900

    itemモデルテストコードの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { 'test' }
+    text                  { 'testtest' }
+    category_id           {2}
+    state_id              {2}
+    dell_price_id         {2}
+    ship_area_id          {2}
+    ship_day_id           {2}
+    price                 {3000}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it '全ての項目が入力されていると登録できるということ' do
+    end
+    it 'imageが空だと登録できない' do
+    end
+    it 'nameが空だと登録できない' do
+    end
+    it 'textが空だと登録できない' do
+    end
+    it 'category_idが空だと登録できない' do
+    end
+    it 'state_idが空だと登録できない' do
+    end
+    it 'dell_priceが空だと登録できない' do
+    end
+    it 'ship_area_idが空だと登録できない' do
+    end
+    it 'ship_day_idが空だと登録できない' do
+    end
+    it 'priceがからだと登録できない' do
+    end
+    it 'nameが40文字以上だと登録できない' do
+    end
+    it 'textが1000文字以上だと登録できない' do
+    end
+    it '価格が300円以下だと登録できない' do
+    end
+    it '価格が9999999円以上だと登録できない' do
+    end
+    it '価格が半角数字じゃないと登録できない' do
+    end
+  end
 end
