dc4e3ac469aacade8d1905a97c552b5df82385a5
syu-akita <syu.akita.0612@gmail.com>
2021-01-04 14:21:45 +0900

    newアクション、createアクションの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :price, :image, :category_id, :state_id, 
+      :dell_price_id, :ship_area_id, :ship_day_id).merge(user_id: current_user.id)
+  end
 end
