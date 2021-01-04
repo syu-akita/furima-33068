52b161008e8cdfe29eb66da3ed816ceb20e7b920
syu-akita <syu.akita.0612@gmail.com>
2021-01-04 12:12:19 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,14 @@ class Item < ApplicationRecord
   belongs_to :dell_price
   belongs_to :ship_area
   belongs_to :ship_day
+
+  validates :name, :text, :price, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :dell_price_id
+    validates :ship_area_id
+    validates :ship_day_id
+  end
 end
