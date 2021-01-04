2953b3b4283e50d619993e34ff606df7b3905fbf
syu-akita <syu.akita.0612@gmail.com>
2021-01-04 19:33:56 +0900

    factortybotにuserの紐付けを行った

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,6 +9,8 @@ FactoryBot.define do
     ship_day_id           {2}
     price                 {3000}
 
+    association :user
+    
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
