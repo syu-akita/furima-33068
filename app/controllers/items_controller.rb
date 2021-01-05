706a26b903bf18beca929ed3565bf895c81186ac
syu-akita <syu.akita.0612@gmail.com>
2021-01-05 19:01:22 +0900

    before_action item_findの記載位置の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: :edit
-  before_action :item_find, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user == @item.user
   end
 
