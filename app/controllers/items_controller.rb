c3c8b76070900f48165021454a10ba6643a50b80
syu-akita <syu.akita.0612@gmail.com>
2021-01-05 19:14:39 +0900

    before_action item_find の中にある:move_to_indexの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:show, :edit, :update, :move_to_index]
+  before_action :item_find, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order(created_at: :DESC)
