0e2e1086df13ee47b26cfdb362d8b0b97b3d124c
syu-akita <syu.akita.0612@gmail.com>
2021-01-05 17:28:59 +0900

    ログインしているユーザーがurlを入力してもindexに飛ばされる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -40,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :price, :image, :category_id, :state_id,
                                  :dell_price_id, :ship_area_id, :ship_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 end
