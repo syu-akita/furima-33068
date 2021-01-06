83d8ad3c8611815d2dc7e788ff627e1766320fd8
syu-akita <syu.akita.0612@gmail.com>
2021-01-06 18:28:25 +0900

    item_orderモデルの実装

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -12,4 +12,9 @@ class ItemOrder
 
   validates :p_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :ship_area, numericality: {other_than: 1}
+
+  def save
+    item = Item.create(name: name, text: text, price: price, category_id: category_id, state_id: state_id, dell_price_id: dell_price_id, ship_area_id: ship_area, ship_day_id: ship_day_id)
+    ShipInfo.create(p_code: p_code, city: city, address: address, phone_num: phone_num, ship_area: ship_area, item_id: item.id)
+  end
 end
