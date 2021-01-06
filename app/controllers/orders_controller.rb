a6d5a6b40899b90073270622067d0a5acc8eea5e
syu-akita <syu.akita.0612@gmail.com>
2021-01-06 18:44:31 +0900

    viewファイルの設定　及　orederコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,18 @@ class OrdersController < ApplicationController
 
   def create
     binding.pry
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:item_order).permit(:p_code, :city, :address, :phone_num, :ship_area)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to        :item
   belongs_to        :user
+  belongs_to        :ship_area
   has_one           :ship_info
 end
