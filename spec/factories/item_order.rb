6ef52e526908fa08da52d80ecaaad5d3f31f3ac1
syu-akita <syu.akita.0612@gmail.com>
2021-01-07 10:57:05 +0900

    FacoryBotの定義

---
+++ spec/factories/item_order.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item_order do
+    p_code                {'350-0027'}
+    city                  {'狭山市'}
+    address               {'さやま町 1-3-4'}
+    phone_num             {'08033994455'}
+    building              {'アパホテル'}
+    ship_area_id          {20}
+  end
+end
