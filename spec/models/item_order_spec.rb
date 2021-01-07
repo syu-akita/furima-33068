f8a52bf4b6a7e38c67ba79883bbe16f8908168c5
syu-akita <syu.akita.0612@gmail.com>
2021-01-07 12:10:56 +0900

    item_orderモデルのテストコードの記述

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -10,26 +10,58 @@ RSpec.describe ItemOrder, type: :model do
       expect(@item_order).to be_valid
     end
     it 'p_codeが空だと登録できない' do
+      @item_order.p_code = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("P code can't be blank")
     end
     it 'ship_area_idが空だと登録できない' do
+      @item_order.ship_area_id = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Ship area can't be blank")
     end
     it 'cityが空だと登録できない' do
+      @item_order.city = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("City can't be blank")
     end
     it 'addressが空だと登録できない' do
+      @item_order.address = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Address can't be blank")
     end
     it 'phone_numが空だと登録できない' do
+      @item_order.phone_num = nil
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone num can't be blank")
     end
     it 'p_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
+      @item_order.p_code = '11111111'
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("P code is invalid")
     end
     it 'phone_numはハイフンがあると登録できない' do
+      @item_order.phone_num = '080-4455-3322'
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone num is invalid")
     end
     it 'phone_numは11桁以内出ないと登録できない' do
+      @item_order.phone_num = '222222222222'
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone num is invalid")
     end
     it 'phone_numは半角英数を含むと登録できない' do
+      @item_order.phone_num = 'abc11223300'
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Phone num is invalid")
     end
     it 'ship_area_idが1だと登録できない' do
+      @item_order.ship_area_id = 1
+      @item_order.valid?
+      expect(@item_order.errors.full_messages).to include("Ship area must be other than 1")
     end
     it 'buildingは空でも登録できる' do
+      @item_order.building = nil
+      expect(@item_order).to be_valid
     end
   end
 end
